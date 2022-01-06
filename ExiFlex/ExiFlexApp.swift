1a36c22bfeb92905f5fac946bfad76d133fcb058
2525mura <2525mura@gmail.com>
2022-01-06 22:48:56 +0900

    Initial Commit

---
+++ ExiFlex/ExiFlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExiFlexApp.swift
+//  ExiFlex
+//
+//  Created by 村井慎太郎 on 2022/01/06.
+//
+
+import SwiftUI
+
+@main
+struct ExiFlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
